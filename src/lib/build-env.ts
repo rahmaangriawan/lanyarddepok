12239066c5385330e05db3588e398d9cb5a3863e
rahmaangriawan <rahmaangriawan78@gmail.com>
2026-06-26 16:18:08 +0700

    Limit DB skip flag to build lifecycle

--- src/lib/build-env.ts
+++ src/lib/build-env.ts
@@ -1,3 +1,6 @@
 export function shouldSkipDbDuringBuild() {
-  return process.env.SKIP_DB_DURING_BUILD === "true";
+  return (
+    process.env.SKIP_DB_DURING_BUILD === "true" &&
+    process.env.npm_lifecycle_event === "build"
+  );
 }
